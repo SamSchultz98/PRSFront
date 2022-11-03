e2f2c7a6fcf4837f2ce52d4c1f328ca34997b6b0
Sam Schultz <sam.schultz@outlook.com>
2022-11-03 12:17:50 -0400

    FormattedMenuForActiveLinkAndVendorDetailTableSet

--- src/app/models/vendor/vendor-detail/vendor-detail.component.ts
+++ src/app/models/vendor/vendor-detail/vendor-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Vendor } from '../vendor.class';
+import { VendorService } from '../vendor.service';
 
 @Component({
   selector: 'app-vendor-detail',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VendorDetailComponent implements OnInit {
 
-  constructor() { }
+  titlePage="Vendor Detail"
+  vend!:Vendor;
+  constructor(
+    private vendsvc: VendorService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.params["id"];
+    this.vendsvc.get(id).subscribe({
+      next:(res)=>{
+        console.log("Employee:",res)
+        this.vend = res
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
   }
 
 }
