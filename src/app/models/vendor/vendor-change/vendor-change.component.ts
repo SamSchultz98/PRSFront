e5d1557236b2f6b9045c4aa8079111dcb2039de2
Sam Schultz <sam.schultz@outlook.com>
2022-11-04 13:43:10 -0400

    LockOutCompleteForVendor

--- src/app/models/vendor/vendor-change/vendor-change.component.ts
+++ src/app/models/vendor/vendor-change/vendor-change.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SystemService } from 'src/app/common/system.service';
 import { Vendor } from '../vendor.class';
 import { VendorService } from '../vendor.service';
 
@@ -15,7 +16,8 @@ export class VendorChangeComponent implements OnInit {
   constructor(
     private vendsvc: VendorService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private syssvc: SystemService
   ) { }
 
   save():void{
@@ -32,6 +34,7 @@ export class VendorChangeComponent implements OnInit {
 
 
   ngOnInit(): void {
+    this.syssvc.verifyUser();
     let id = +this.route.snapshot.params["id"];
     this.vendsvc.get(id).subscribe({
       next: (res) => {
