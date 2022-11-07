a038836e2536df885e4d693fb01d5242800f0db3
Sam Schultz <sam.schultz@outlook.com>
2022-11-07 14:22:51 -0500

    RequestListComplete

--- src/app/models/request/request-list/request-list.component.ts
+++ src/app/models/request/request-list/request-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SystemService } from 'src/app/common/system.service';
+import { RequestService } from '../request.service';
+import { Request } from '../request.class';
 
 @Component({
   selector: 'app-request-list',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestListComponent implements OnInit {
 
-  constructor() { }
+  pageTitle:string="Requests List"
+  reqs:Request[]=[];
+  constructor(
+    private reqsvc: RequestService,
+    private syssvc: SystemService
+  ) { }
 
   ngOnInit(): void {
+    this.reqsvc.list().subscribe({
+      next: (res) => {
+        console.log("Requests:",res)
+        this.reqs = res
+      },
+      error: (err) =>{
+        console.error(err);
+      }
+    })
+  }
   }
 
-}
+
--- src/app/models/request/request.class.ts
+++ src/app/models/request/request.class.ts
@@ -1,10 +1,13 @@
+import { User } from "../user/user.class";
+
 export class Request{
     id:number=0;
     description:string="";
     justification:string="";
-    rejectionreason:string="";
-    delieverymode:string="Pickup";
+    rejectionReason:string="";
+    delieveryMode:string="Pickup";
     status:string="NEW";
     total:number=0;
     userid:string="";
+    user!:User;
 }
