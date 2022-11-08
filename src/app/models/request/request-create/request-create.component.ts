ad3600adf25f63ff339ef990e83e7028ab3e707e
Sam Schultz <sam.schultz@outlook.com>
2022-11-08 09:55:22 -0500

    GraphicalFixes

--- src/app/models/request/request-create/request-create.component.ts
+++ src/app/models/request/request-create/request-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RequestService } from '../request.service';
+import { Request } from '../request.class';
 
 @Component({
   selector: 'app-request-create',
@@ -6,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-create.component.css']
 })
 export class RequestCreateComponent implements OnInit {
+  showVerifyButton:boolean = false;
+  titlePage="Request Edit";
+  req: Request = new Request;
 
-  constructor() { }
+  constructor(
+    private reqsvc: RequestService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
+  save():void{
+    this.reqsvc.change(this.req).subscribe({
+      next:(res) => {
+        console.debug("Request Changed")
+        this.router.navigateByUrl("/Requests")
+      },
+      error:(err)=>{
+        console.error(err);
+      }
+    })
+  }
   ngOnInit(): void {
   }
 
