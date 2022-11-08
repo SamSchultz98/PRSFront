aca8d972fb5f850b8b59beb6304bd9fccae90fdb
Sam Schultz <sam.schultz@outlook.com>
2022-11-08 16:12:58 -0500

    AddingNewRequestLineNowNavigatesBackToLines

--- src/app/models/requestline/requestline-create/requestline-create.component.ts
+++ src/app/models/requestline/requestline-create/requestline-create.component.ts
@@ -28,7 +28,7 @@ export class RequestlineCreateComponent implements OnInit {
     this.reqlsvc.create(this.reql).subscribe({
       next:(res)=>{
         console.log(res)
-        this.router.navigateByUrl("/Requests/lines/:id")
+        this.router.navigateByUrl(`/Requests/lines/${this.reql.requestId}`)
       }
     })
   }
