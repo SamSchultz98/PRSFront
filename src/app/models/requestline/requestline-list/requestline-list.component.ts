ee422446b46bddd1b575fe1a104dfab97f777cfd
Sam Schultz <sam.schultz@outlook.com>
2022-11-08 14:41:53 -0500

    SubmitForReviewWorking

--- src/app/models/requestline/requestline-list/requestline-list.component.ts
+++ src/app/models/requestline/requestline-list/requestline-list.component.ts
@@ -28,12 +28,33 @@ review():void{
   this.reqlsvc.review(this.req).subscribe({
     next:(res)=>{
       console.log(res)
+      this.refresh()
     },
     error:(err)=>{
       console.error(err);
     }
   })
 }
+
+refresh():void{
+let reqid = +this.route.snapshot.params["id"];
+  this.reqsvc.get(reqid).subscribe({
+    next:(res)=>{
+      console.log(res)
+      this.req = res
+    }
+  })
+  this.reqlsvc.list().subscribe({
+    next: (res) => {
+      console.log("Requestlines:",res)
+      this.reqlines = res;
+    },
+    error: (err) => {
+      console.error(err)
+    }
+  })
+}
+
 
   ngOnInit(): void {
     let reqid = +this.route.snapshot.params["id"];
