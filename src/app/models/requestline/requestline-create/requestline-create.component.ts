a555a0e58f02f7b1a0744019dd1a8f85eb895989
Sam Schultz <sam.schultz@outlook.com>
2022-11-08 16:00:02 -0500

    RLCreateDoneNeedItToNavigateToBackToTheRightLine

--- src/app/models/requestline/requestline-create/requestline-create.component.ts
+++ src/app/models/requestline/requestline-create/requestline-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../../product/product.class';
+import { ProductService } from '../../product/product.service';
+import { Requestline } from '../requestline.class';
+import { RequestlineService } from '../requestline.service';
 
 @Component({
   selector: 'app-requestline-create',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestlineCreateComponent implements OnInit {
 
-  constructor() { }
+
+  Pagetitle:string="Create Request Line"
+  reql:Requestline = new Requestline;
+  prods:Product[] = [];
+  reqid:Number=this.reql.requestId
+  constructor(
+    private reqlsvc: RequestlineService,
+    private prodsvc: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  save(): void {
+    this.reqlsvc.create(this.reql).subscribe({
+      next:(res)=>{
+        console.log(res)
+        this.router.navigateByUrl("/Requests/lines/:id")
+      }
+    })
+  }
+
 
   ngOnInit(): void {
+    this.prodsvc.list().subscribe({
+      next:(res)=>{
+        console.log(res)
+        this.prods=res
+      },
+      error:(err)=>{
+        console.debug(err)
+      }
+    })
+    
+    this.reql.requestId = +this.route.snapshot.params["id"];
   }
 
 }
