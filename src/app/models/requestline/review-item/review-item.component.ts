7a5cc74fe84a9099a00481e9260fd7338fa51b86
Sam Schultz <sam.schultz@outlook.com>
2022-11-09 12:10:59 -0500

    ApproveFunctionActive

--- src/app/models/requestline/review-item/review-item.component.ts
+++ src/app/models/requestline/review-item/review-item.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../product/product.service';
+import { RequestService } from '../../request/request.service';
+import { Requestline } from '../requestline.class';
+import { RequestlineService } from '../requestline.service';
+import { Request } from '../../request/request.class';
 
 @Component({
   selector: 'app-review-item',
@@ -7,9 +13,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewItemComponent implements OnInit {
 
-  constructor() { }
+  pageTitle:string="Request Lines"
+  reqlines: Requestline[] = [];
+  req!: Request;
+  reql!:Requestline;
+
+  constructor(
+    private reqlsvc:RequestlineService,
+    private reqsvc:RequestService,
+    private prodsvc:ProductService,
+    private route: ActivatedRoute,
+  ) { }
+
+    accept():void{
+      this.req.status = "APPROVED"
+      this.reqsvc.change(this.req).subscribe({
+        next:(res)=>{
+          console.log(res)
+          this.refresh()
+        },
+        error:(err)=>{
+          console.error(err)
+        }
+      })
+    }
+
+    refresh(): void{
+      let reqid = +this.route.snapshot.params["id"];
+    this.reqsvc.get(reqid).subscribe({
+      next:(res)=>{
+        console.log(res)
+        this.req = res
+      },
+      error:(err)=>{
+        console.error(err)
+      }
+    })
+    this.reqlsvc.list().subscribe({
+      next: (res) => {
+        console.log("Requestlines:",res)
+        this.reqlines = res;
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
+  }
 
   ngOnInit(): void {
+    let reqid = +this.route.snapshot.params["id"];
+    this.reqsvc.get(reqid).subscribe({
+      next:(res)=>{
+        console.log(res)
+        this.req = res
+      },
+      error:(err)=>{
+        console.error(err)
+      }
+    })
+    this.reqlsvc.list().subscribe({
+      next: (res) => {
+        console.log("Requestlines:",res)
+        this.reqlines = res;
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
   }
 
 }
